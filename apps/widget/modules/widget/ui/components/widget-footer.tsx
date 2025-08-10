e54309b93231e273ca1e0b56bc8c947af21e90b5
alexander flores <alexfloresrayme@gmail.com>
2025-08-10 11:26:08 -0500

    fixed(widget-footer): integrate state management for screen selection in footer buttons

--- apps/widget/modules/widget/ui/components/widget-footer.tsx
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -1,18 +1,20 @@
 import { Button } from "@workspace/ui/components/button";
 import { cn } from "@workspace/ui/lib/utils";
+import { useAtomValue, useSetAtom } from "jotai";
 import { HomeIcon, InboxIcon } from "lucide-react";
+import { screenAtom } from "../../atoms/widget-atoms";
 
 export const WidgetFooter = () => {
-  const screen = "selection";
+  const screen = useAtomValue(screenAtom);
+  const setScreen = useSetAtom(screenAtom);
+
   return (
     <footer className="flex items-center justify-between border-t bg-background">
       <Button
         className="h-14 flex-1 rounded-none"
-        onClick={() => {
-          /* Handle click */
-        }}
-        variant="ghost"
+        onClick={() => setScreen("selection")}
         size="icon"
+        variant="ghost"
       >
         <HomeIcon
           className={cn("size-5", screen === "selection" && "text-primary")}
@@ -20,11 +22,9 @@ export const WidgetFooter = () => {
       </Button>
       <Button
         className="h-14 flex-1 rounded-none"
-        onClick={() => {
-          /* Handle click */
-        }}
-        variant="ghost"
+        onClick={() => setScreen("inbox")}
         size="icon"
+        variant="ghost"
       >
         <InboxIcon
           className={cn("size-5", screen === "inbox" && "text-primary")}
